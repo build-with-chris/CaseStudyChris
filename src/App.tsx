f35a86fda9e741d59916e1a15767967ef8787a79
hecking_killa <chris.hermann9397@gmail.com>
2025-07-11 10:01:59 +0200

    fullfilling all the requirements

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import ProgressBar from "./ProgressBar.tsx"
 
 function App() {
-  const cartValue = 100
+  const cartValue = 25
 
   return (
     <>
+    <h3 className="text-center text-3xl text-[#002F6C] mt-5 font-sans">Ihr Warenkorb</h3>
       <div className="mt-5">
         <ProgressBar cartValue={cartValue}/>
         </div>
--- src/ProgressBar.tsx
+++ src/ProgressBar.tsx
@@ -5,7 +5,7 @@ interface ProgressBarProps {
 }
 
 const FREE_SHIPPING = 25
-const GIFT = 100
+const GIFT = 75
 
 function calculateProgress(cartValue: number){
     if (cartValue <= FREE_SHIPPING) {
@@ -15,14 +15,45 @@ function calculateProgress(cartValue: number){
     } else {
         return 100;
     }
+}
 
+function getTopText(cartValue: number): JSX.Element {
+  if (cartValue < FREE_SHIPPING) {
+    const remaining = (FREE_SHIPPING - cartValue).toFixed(2);
+    return <>Nur noch {remaining} € bis zum kostenlosen Versand!</>;
+  } else if (cartValue < GIFT) {
+    const remaining = (GIFT - cartValue).toFixed(2);
+    return <>Nur noch {remaining} € bis zu deinem Gratisprodukt!</>;
+  } else {
+    return <span className="text-green-700">Sie erhalten Geschenk und Versand kostenlos</span>;
+  }
+}
+
+function getBottomText(cartValue: number): string {
+  if (cartValue < FREE_SHIPPING) {
+    return "Du bist auf dem besten Weg!";
+  } else if (cartValue < GIFT) {
+    return "Versand gesichert – weiter so!";
+  } else {
+    return "";
+  }
 }
 
 
 export default function ProgressBar({ cartValue }: ProgressBarProps) {
     const progress = calculateProgress(cartValue)
+    const topText = getTopText(cartValue);
+    const bottomText = getBottomText(cartValue);
 
-    return(
-    <Progress value={progress} />
-    )
+    return (
+      <div className="bg-[#F0F4F6] p-5 text-center">
+        
+        <p className="p-1 text-md mb-4 text-primary/50" >{topText}</p>
+        <Progress value={progress} />
+        {bottomText && 
+        (<p className="p-1 text-md mt-4 text-primary/50">{bottomText}</p>)
+        }
+        
+      </div>
+    );
 }
